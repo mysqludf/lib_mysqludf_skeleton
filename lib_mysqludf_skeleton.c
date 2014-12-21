38c5e51e2850bec7ee39d7dff9a7b3138781f98a
Daniel Black <daniel.black@openquery.com>
2014-12-21 21:59:01 +1100

    more notes about aggregation and init functions

--- lib_mysqludf_skeleton.c
+++ lib_mysqludf_skeleton.c
@@ -54,13 +54,57 @@ extern "C" {
  * lib_mysqludf_skeleton_info()
  */
 
+/* Sample data for aggregate function */
+struct aggregate_data {
+  ulonglong count;
+};
+
+/* The init function must be the name of the function with '_init' appended afterwards */
+/* Return != 0 for an error and fill *message with an error messages if required
+ * If successfull return 0 */
 my_bool lib_mysqludf_skeleton_info_init(UDF_INIT *initid, UDF_ARGS *args, char *message)
 {
+  /* for simple functions should validate the number and type of arguements are correct. */
+  if (args->arg_count != 0)
+  {
+    strncpy(message,"wrong number of arguments: lib_mysqludf_skeleton_info() requires no arguments", MYSQL_ERRMSG_SIZE);
+    return 1;
+  }
+	/*
+	if (args->arg_type[0]!= INT_RESULT)
+	{
+		strncpy(message,"lib_mysqludf_skeleton_info_init() requires an int as parameter", MYSQL_ERRMSG_SIZE);
+		return 1;
+	}
+	*/
+
+	initid->maybe_null  = 0;    /* The result may be null */
+	initid->decimals  = 4;    /* We want 4 decimals in the result */
+	initid->max_length  = 20;   /* 6 digits + . + 10 decimals */
+
+	/* For aggregate functions memory will need to be allocated for progress results.
+			use the initd->ptr pointer for successive uses */
+	/*
+	struct aggregate_data *data;
+	if (!(data = (struct aggregate_data*) malloc(sizeof(struct avgcost_data))))
+	{
+		strmov(message,"Couldn't allocate memory");
+		return 1;
+	}
+	data->count = 0L;
+
+	initid->ptr = data;
+	*/
 	return 0;
 }
 
+/* This is required to free any memory alloced in _init */
 void lib_mysqludf_skeleton_info_deinit(UDF_INIT *initid)
 {
+	if (initid != NULL && initid->ptr != NULL) {
+		free(initid->ptr);
+		initid->ptr = NULL;
+	}
 }
 
 /* For functions that return REAL */
@@ -74,4 +118,23 @@ char* lib_mysqludf_skeleton_info(UDF_INIT *initid, UDF_ARGS *args, char* result,
 	strcpy(result, PACKAGE_STRING);
 	*length = strlen(PACKAGE_STRING);
 	return result;
+}
+
+/* clear is called between each group including before the first group in the aggregation */
+void lib_mysqludf_skeleton_info_clear( UDF_INIT* initid, char* is_null, char* is_error )
+{
+	struct aggregate_data *data = (struct aggregate_data*) initid->ptr;
+	data->count = 0L;
+  /* Watch out for http://bugs.mysql.com/bug.php?id=38297 / MariaDB MDEV-7357 when using aggregation and WITH ROLLUP *
+ *  you may need to allocation memory here in clear and deallocate in lib_mysqludf_skeleton_info. Check initid->ptr
+ *  value first in case it gets fixed in future */
+}
+
+/* add is called for each row in the group */
+void lib_mysqludf_skeleton_info_add( UDF_INIT* initid, UDF_ARGS* args, char* is_null, char* is_error )
+{
+	struct aggregate_data *data = (struct aggregate_data*) initid->ptr;
+	if (args->args[0]) {
+		data->count += *((longlong*)args->args[0]);
+	}
 }
